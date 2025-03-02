3142ff2044abf640a94940c82627fdf0ec47554f
idkwhatiamdoingwithmylife <braedeng482@gmail.com>
2025-03-02 00:21:08 -0500

    Update encode.js

--- api/encode.js
+++ api/encode.js
@@ -5,18 +5,15 @@ function sendEncodedIP() {
         return;
     }
 
-    const base64EncodedIP = btoa(ip);
-    console.log("Base64 Encoded IP:", base64EncodedIP);
-
-    const binaryEncodedIP = toBinary(base64EncodedIP);
-    console.log("Binary Encoded IP:", binaryEncodedIP);
+    const encodedIP = btoa(ip);
+    console.log("Encoded IP:", encodedIP);
 
     fetch("https://dont-look-here.vercel.app/api/api.js", {
         method: "POST",
         headers: {
             "Content-Type": "application/json"
         },
-        body: JSON.stringify({ encodedIP: binaryEncodedIP })
+        body: JSON.stringify({ encodedIP })
     })
     .then(response => response.json())
     .then(data => {
@@ -25,12 +22,4 @@ function sendEncodedIP() {
     .catch(error => {
         console.error("Error:", error);
     });
-}
-
-function toBinary(str) {
-    let binary = '';
-    for (let i = 0; i < str.length; i++) {
-        binary += str.charCodeAt(i).toString(2).padStart(8, '0'); // Convert each char to binary
-    }
-    return binary;
 }
